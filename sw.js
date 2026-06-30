58b2621a39efeabfb8e68e4818304936b2addfb2
mariuspodiuc <60138257+mariuspodiuc@users.noreply.github.com>
2026-06-30 16:03:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='ncp-pro-v1';
-const FILES=['./','./index.html','./style.css','./app.js','./logo.jpg','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
